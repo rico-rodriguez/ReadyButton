8a3503c82811e04252dfc4fdae7d04f389d6c796
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-16 06:22:33 +0900

    fetchdata extracted to new useEffect

--- src/ButtonClicker.js
+++ src/ButtonClicker.js
@@ -96,33 +96,34 @@ export default function ButtonClicker() {
       console.error('Error resetting click count:', err);
     }
   }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true); // show loading spinner
+        const response = await fetch(`https://readybutton.herokuapp.com/api/button/${urlId}`, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch button data');
+        }
+        if (response.status === 404) {
+          //Button not found, create new button on server side
+        } else {
+          const data = await response.json();
+          setButtonData(data);
+          setLoading(false); // hide loading spinner
+        }
+      } catch (error) {
+        console.error('Error fetching button data:', error);
+      }
+    }
+    fetchData();
+  }, [urlId]);
   // Only make requests when userId is not null and loading is false
   useEffect(() => {
     if (userId && !loading) {
-      async function fetchData() {
-        try {
-          const response = await fetch(
-            `https://readybutton.herokuapp.com/api/button/${urlId}`,
-            {
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-              },
-            }
-          );
-          if (!response.ok) {
-            throw new Error('Failed to fetch button data');
-          }
-          if (response.status === 404) {
-            //Button not found, create new button on server side
-          } else {
-            const data = await response.json();
-            setButtonData(data);
-          }
-        } catch (error) {
-          console.error('Error fetching button data:', error);
-        }
-      }
-      fetchData();
       socket.on('snackbar', (data) => {
         setSnackbarOpen(true);
       });
