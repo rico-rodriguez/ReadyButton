30c66b53a91d4d2ce64e4564df23ada05b18583d
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-17 04:47:25 +0900

    Home style reconfigured

--- src/Home.js
+++ src/Home.js
@@ -1,123 +1,99 @@
-import socketClient from 'socket.io-client';
-import { Box, Checkbox, Container, Typography } from '@mui/material';
-import { makeStyles, withStyles } from '@mui/styles';
+import {Box, Checkbox, Container, css, Typography} from '@mui/material';
+import * as PropTypes from "prop-types";
 const io = require('socket.io-client');
 const socket = io('https://readybutton.herokuapp.com/', {
   withCredentials: false,
 });
 socket.on('connect', () => {
-  console.log(`I'm connected with thesocket from home`);
   socket.emit('connection', null);
 });
 
-const useHomeStyle = makeStyles(
-  (theme) => ({
-    root: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignSelf: 'center',
-      justifyItems: 'space-between',
-      alignItems: 'space-between',
-      padding: '20px',
-      background: 'rgba(0, 0, 0, 0.5)',
-      boxShadow:
-        '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
-      borderRadius: '5px',
-      fontFamily: 'Open Sans, sans-serif',
-      margin: 'auto',
-      marginTop: '10px',
-        marginBottom: '10px',
-      height: '100%',
-      zIndex: 999,
-    },
+function CustomCheckbox(props) {
+  const { checked, onChange, ...other } = props;
 
-    header: {
-      fontWeight: 'bold',
-      textTransform: 'uppercase',
-      color: 'white',
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      fontSize: '4rem',
-    },
-    subheader: {
-      backgroundColor: 'rgba(96,96,96,0.15)',
-      color: '#eeeeee',
-      padding: theme.spacing(2),
-      fontFamily: 'Source Code Pro, monospace',
-      fontSize: '14px',
-      borderRadius: '5px',
-    },
-    featureList: {
-      listStyle: 'none',
-      padding: 0,
-      margin: 0,
-    },
-    featureItem: {
-      display: 'flex',
-      alignItems: 'center',
-      padding: theme.spacing(1),
-      color: 'white',
-    },
-    featureListContainer: {
-      overflowY: 'scroll',
-      '&::-webkit-scrollbar': {
-        width: '10px',
-        backgroundColor: '#747e94',
-      },
-      '&::-webkit-scrollbar-thumb': {
-        backgroundColor: '#1c2536',
-      },
-    },
-    customCheckbox: {
-      width: '20px',
-      height: '20px',
-      marginRight: '10px',
-      '& .MuiSvgIcon-root': {
-        color: '#5c698c',
-        marginRight: '10px',
-        '&.Mui-unchecked': {
-          color: '#ffffff'
-        },
-        '&.Mui-checked': {
-          color: '#1aa400',
-        },
-      }
-    },
-  }),
-  { name: 'home' }
-);
-
-export default function Home() {
-  const CustomCheckbox = withStyles(useHomeStyle())((props) => (
-    <Checkbox color='default' {...props} />
-  ));
-  const classes = useHomeStyle();
-  return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+return (
+    <Box
+        sx={{
+            display: 'flex',
+            alignItems: 'center',
+            '& > :not(style)': { m: 1 },
         }}
-      >
+  >
+    <Checkbox
+        checked={checked}
+        onChange={onChange}
+        {...other}
+    />
+    <Typography variant="body2" color="text.secondary">
+        {props.label}
+    </Typography>
+    </Box>
+    );
+}
+CustomCheckbox.propTypes = {
+  onChange: PropTypes.func,
+  checked: PropTypes.bool,
+  className: PropTypes.any
+};
+export default function Home() {
+  return (
+      <>
+        <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+        >
 
-        <Container maxWidth='sm' className={classes.root}>
-          <Typography variant='h2' className={classes.header}>
-            Welcome
-          </Typography>
-          <Box className={classes.subheader}>
-            <Typography variant='subtitle1'>
-              To get started, add anything after the '/' of this url and you
-              will be sent to your own instance of the ready button. Share the
+          <Container maxWidth='sm' style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignSelf: 'center',
+            justifyItems: 'space-between',
+            alignItems: 'space-between',
+            padding: '20px',
+            background: 'rgba(0, 0, 0, 0.5)',
+            boxShadow:
+                '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
+            borderRadius: '5px',
+            fontFamily: 'Open Sans, sans-serif',
+            margin: 'auto',
+            marginTop: '10px',
+            marginBottom: '10px',
+            height: '100%',
+            zIndex: 999,
+          }}>
+            <Typography variant='h2' style={{
+              fontWeight: 'bold',
+              textTransform: 'uppercase',
+              color: 'white',
+              padding: '20px',
+              textAlign: 'center',
+              fontSize: '4rem',
+            }}>
+              Welcome
+            </Typography>
+            <Box style={{
+              backgroundColor: 'rgba(96,96,96,0.15)',
+              color: '#eeeeee',
+              padding: '20px',
+              fontFamily: 'Source Code Pro, monospace',
+              fontSize: '14px',
+              borderRadius: '5px',
+            }}>
+              <Typography variant='subtitle1'>
+                To get started, add anything after the '/' of this url and you
+                will be sent to your own instance of the ready button. Share the
               link with classmates or friends to see when everyone is ready!
             </Typography>
           </Box>
-          <div className={classes.featureListContainer} style={{height: "20rem", overflowY: "scroll"}}>
-          <ul className={classes.featureList}>
-            <li className={classes.featureItem}>
+          <div className="featureListContainer">
+          <ul className="featureList">
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={false}
                 onChange={() => {}}
               />
@@ -126,9 +102,9 @@ export default function Home() {
                 notifies when it is reached
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={false}
                 onChange={() => {}}
               />
@@ -136,9 +112,9 @@ export default function Home() {
                 Add Emoji burst from cursor on click
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={false}
                 onChange={() => {}}
               />
@@ -146,9 +122,9 @@ export default function Home() {
                 Restrict URL's that can be made, clean periodically
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={false}
                 onChange={() => {}}
               />
@@ -157,9 +133,9 @@ export default function Home() {
                 who have clicked
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={false}
                 onChange={() => {}}
               />
@@ -167,9 +143,9 @@ export default function Home() {
                 Integrate with Teams via webhooks/client bot
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={true}
                 onChange={() => {}}
               />
@@ -177,9 +153,9 @@ export default function Home() {
                 Integrate websockets for incrementing button/resetting
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                  className={classes.customCheckbox}
+                  className="customCheckbox"
                   checked={true}
                   onChange={() => {}}
               />
@@ -187,9 +163,9 @@ export default function Home() {
                 Add Loading spinner that sends signals out via websockets
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={true}
                 onChange={() => {}}
               />
@@ -197,17 +173,17 @@ export default function Home() {
                 Add Snackbar for button reset
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={true}
                 onChange={() => {}}
               />
               <Typography variant='subtitle2'>Enable Reset button</Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={true}
                 onChange={() => {}}
               />
@@ -215,9 +191,9 @@ export default function Home() {
                 Migrate to web and modify localhost to web addresses.
               </Typography>
             </li>
-            <li className={classes.featureItem}>
+            <li className="featureItem">
               <CustomCheckbox
-                className={classes.customCheckbox}
+                className="customCheckbox"
                 checked={true}
                 onChange={() => {}}
               />
