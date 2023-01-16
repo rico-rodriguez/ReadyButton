750f5c1581d92c631da4cf4c9a592606563982db
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-16 10:06:34 +0900

    t/o anim

--- src/ButtonClicker.js
+++ src/ButtonClicker.js
@@ -163,6 +163,7 @@ export default function ButtonClicker() {
         >
           {loading ? (
             <CircularProgress
+              timeout={1500}
               color='success'
               style={{
                 position: 'absolute',
