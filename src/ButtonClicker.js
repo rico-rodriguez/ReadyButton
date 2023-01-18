a282cfee734178c640d87a93b36b583b7bb298f4
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-19 01:19:24 +0900

    sparkle animation uupdates

--- src/ButtonClicker.js
+++ src/ButtonClicker.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import mojs from '@mojs/core';
 import {
@@ -50,11 +50,13 @@ export default function ButtonClicker() {
   }, []);
 
   async function handleClick() {
-    {animatedClick()}
     setEmojiVisible(true);
+    {
+      animatedClick();
+    }
     setTimeout(() => {
       setEmojiVisible(false);
-    }, 5000);
+    }, 3000);
     // Check if user has already clicked the button
     if (!clickedUsers.includes(urlId) && userId) {
       setClickedUsers([...clickedUsers, urlId]);
@@ -93,7 +95,7 @@ export default function ButtonClicker() {
         }
       );
       const data = await response.json();
-    setDataLoaded(false);
+      setDataLoaded(false);
       setButtonData(data);
       setClickedUsers([]);
       setDataLoaded(true);
@@ -107,7 +109,9 @@ export default function ButtonClicker() {
     async function fetchData() {
       setDataLoaded(false);
       try {
-        const response = await fetch(`https://readybutton.herokuapp.com/api/button/${urlId}`);
+        const response = await fetch(
+          `https://readybutton.herokuapp.com/api/button/${urlId}`
+        );
         if (!response.ok) {
           throw new Error('Failed to fetch button data');
         }
@@ -124,55 +128,88 @@ export default function ButtonClicker() {
     socket.on('increment', (data) => {
       snackbarOpen ? setSnackbarOpen(false) : setSnackbarOpen(true);
       dataLoaded && setButtonData({ count: data.count });
-    fetchData();
+      fetchData();
     });
     socket.on('reset', (data) => {
-      snackbarOpenReset ? setSnackbarOpenReset(false) : setSnackbarOpenReset(true);
+      snackbarOpenReset
+        ? setSnackbarOpenReset(false)
+        : setSnackbarOpenReset(true);
       dataLoaded && setButtonData({ count: data.count });
       fetchData();
     });
   }, [urlId]);
-
+  class Heart extends mojs.CustomShape {
+    getShape() {
+      return '<path d="M73.6170213,0 C64.4680851,0 56.5957447,5.53191489 51.7021277,13.8297872 C50.8510638,15.3191489 48.9361702,15.3191489 48.0851064,13.8297872 C43.4042553,5.53191489 35.3191489,0 26.1702128,0 C11.9148936,0 0,14.0425532 0,31.2765957 C0,48.0851064 14.893617,77.8723404 47.6595745,99.3617021 C49.1489362,100.212766 50.8510638,100.212766 52.1276596,99.3617021 C83.8297872,78.5106383 99.787234,48.2978723 99.787234,31.2765957 C100,14.0425532 88.0851064,0 73.6170213,0 L73.6170213,0 Z"></path>';
+    }
+  }
+  mojs.addShape('heart', Heart);
   const animatedClick = () => {
     if (!animationRef.current) {
       animationRef.current = new mojs.Burst({
         parent: '#my-div',
-        radius:   { 150: 200 },
-        angle:    45,
-        count:    14,
-        timeline: { delay: 0 },
+        radius: { 150: 200 },
+        angle: 45,
+        count: 40,
+        timeline: { delay: 300 },
         children: {
-          radius:       2.5,
-          fill:         [
-            { '#9EC9F5' : '#9ED8C6' },
-            { '#91D3F7' : '#9AE4CF' },
+          radius: 4.5,
+          fill: [
+            // { '#91D2FA' : '#BDEFD8' },
+            // { '#91D2FA' : '#ADD6CA' },
+            { '#9EC9F5': '#9ED8C6' },
+            { '#91D3F7': '#9AE4CF' },
 
-            { '#DC93CF' : '#E3D36B' },
-            { '#CF8EEF' : '#CBEB98' },
+            { '#DC93CF': '#E3D36B' },
+            { '#CF8EEF': '#CBEB98' },
 
-            { '#87E9C6' : '#1FCC93' },
-            { '#A7ECD0' : '#9AE4CF' },
+            { '#87E9C6': '#1FCC93' },
+            { '#A7ECD0': '#9AE4CF' },
 
-            { '#87E9C6' : '#A635D9' },
-            { '#D58EB3' : '#E0B6F5' },
+            { '#87E9C6': '#A635D9' },
+            { '#D58EB3': '#E0B6F5' },
 
-            { '#F48BA2' : '#CF8EEF' },
-            { '#91D3F7' : '#A635D9' },
+            { '#F48BA2': '#CF8EEF' },
+            { '#91D3F7': '#A635D9' },
 
-            { '#CF8EEF' : '#CBEB98' },
-            { '#87E9C6' : '#A635D9' },
+            { '#CF8EEF': '#CBEB98' },
+            { '#87E9C6': '#A635D9' },
           ],
-          scale:        { 1: 0, easing: 'quad.in' },
-          pathScale:    [ .8, null ],
-          degreeShift:  [ 2, null ],
-          duration:     [ 500, 1000 ],
-          easing:       'quint.out'
-        }
+          scale: { 1: 0, easing: 'quad.in' },
+          pathScale: [0.8, null],
+          degreeShift: [13, null],
+          duration: [500, 700],
+          easing: 'quint.out',
+          speed: 0.7,
+        },
       }).play();
-    } else {
-      animationRef.current.play();
+      const CIRCLE_RADIUS = 150;
+      animationRef.current = new mojs.Shape({
+        parent: '#my-div',
+        stroke: { '#344c6a': '#d5cccc' },
+        strokeWidth: { [45]: 0 },
+        fill: 'none',
+        scale: { 0: 1 },
+        duration: 400,
+        radius: CIRCLE_RADIUS,
+        easing: 'cubic.out',
+      }).play();
+      // animationRef.current = new mojs.Shape({
+      //   parent: '#my-div',
+      //   shape: 'heart',
+      //   fill: '#E5214A',
+      //   scale: { 0: 1 },
+      //   duration: [1000],
+      //   radius: 70,
+      //   easing: 'elastic.out',
+      //   delay: 300,
+      //   isShowEnd: false,
+      // }).play();
+      // setTimeout(() => {
+      //   animationRef.current.stop();
+      // }, 3000);
     }
-    }
+  };
   return (
     <div>
       <Grid
@@ -184,41 +221,41 @@ export default function ButtonClicker() {
         style={{ minHeight: '100vh' }}
       >
         <div>
-        <Button
-            id="my-div"
+          <Button
+            id='my-div'
             style={{
-            width: '3em',
-            height: '3em',
-            fontSize: '90px',
-            borderRadius: '9999px',
-            marginBottom: '20px',
-            filter: 'drop-shadow(5px 5px 10px #000)',
-          }}
-            disabled={clickedUsers.includes(urlId)}
-          color='primary'
-          variant='contained'
-          onClick={handleClick}
+              width: '3em',
+              height: '3em',
+              fontSize: '90px',
+              borderRadius: '9999px',
+              marginBottom: '20px',
+              filter: 'drop-shadow(5px 5px 10px #000)',
+            }}
+            disabled={clickedUsers.includes(userId) || !buttonData.count}
+            color='primary'
+            variant='contained'
+            onClick={handleClick}
             backgroundColor='primary'
-        >
-          {clickedUsers.includes(urlId) && <div className="completedClick"></div>}
+          >
+            {clickedUsers.includes(userId) ||
+              (!buttonData.count && <div className='completedClick'></div>)}
 
-          {!dataLoaded ? (
-            <CircularProgress
-              color='success'
-              style={{
-                position: 'absolute',
-                width: '3em',
-                height: '3em',
-                fontSize: '90px',
-              }}
-            />
-          ) : (
-            buttonData.count
-          )}
-        </Button>
-            {emojiVisible && <div>
-              </div>}
-      </div>
+            {!dataLoaded ? (
+              <CircularProgress
+                color='success'
+                style={{
+                  position: 'absolute',
+                  width: '3em',
+                  height: '3em',
+                  fontSize: '90px',
+                }}
+              />
+            ) : (
+              buttonData.count
+            )}
+          </Button>
+          {emojiVisible && <div></div>}
+        </div>
         <Button color='neutral' variant='contained' onClick={handleReset}>
           Reset
         </Button>
@@ -233,10 +270,11 @@ export default function ButtonClicker() {
           </Alert>
         </Snackbar>
         <Snackbar
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-            autoHideDuration={3000}
-            open={snackbarOpenReset}
-            onClose={() => setSnackbarOpenReset(false)}>
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          autoHideDuration={3000}
+          open={snackbarOpenReset}
+          onClose={() => setSnackbarOpenReset(false)}
+        >
           <Alert severity='error' sx={{ width: '100%' }}>
             Button count reset!
           </Alert>
