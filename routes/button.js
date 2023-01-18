66ab2e990365efe04f6b8cfdc15604107435a932
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-18 12:11:48 +0900

    965

---
+++ routes/button.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const buttonRoutes = express.Router();
+const uuid = require('uuid');
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = process.env.ATLAS_URI;
+const cookieParser = require('cookie-parser');
+
+// buttonRoutes.route('/').post(async function () {
+//     console.log('POST /');
+//     const newButton = new Button({
+//         urlId: "rico",
+//         count: 0
+//     });
+//     const client = new MongoClient(connectionString, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     });
+//     await client.connect(err => {
+//         const collection = client.db("button").collection("buttons");
+//         collection.insertOne(newButton, function (err, res) {
+//             if (err) throw err;
+//             console.log("1 document inserted");
+//             client.close();
+//         });
+//     });
+// });
+
+//initial page load
+buttonRoutes.route('/api/user/id').get(async (req, res) => {
+    const client = new MongoClient(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    await client.connect(async err => {
+        let userId = req.cookies.userId;
+        console.log('userId: ' + userId);
+        if (!userId) {
+            userId = uuid.v4();
+            res.cookie('userId', userId, {
+                maxAge: 9000000, // expires in 15 minutes
+                httpOnly: true
+            });
+        }
+        res.send({ userId });
+        await client.close();
+    });
+});
+buttonRoutes.route("/api/button/:urlId").get(async (req, res) => {
+    const client = new MongoClient(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    await client.connect(async err => {
+        const collection = client.db("button").collection("buttons");
+        let button = await collection.findOne({ urlId: req.params.urlId });
+        if (!button) {
+            console.log('Button not found, creating a new one');
+            let userId = req.cookies.userId;
+            button = {
+                urlId: req.params.urlId,
+                count: 0,
+                usersArray: [userId,]
+            };
+            console.log('Button:', button);
+            await collection.insertOne(button);
+        }
+        res.json({ count: button.count });
+        console.log('Button count:', button.count);
+        console.log('Button count sent to the client')
+        await client.close();
+    });
+});
+
+buttonRoutes.route('/api/button/increment/:urlId')
+    .patch(async (req, res) => {
+        try {
+        } catch (err) {
+            console.error('Error setting loading spinner:', err);
+        }
+
+        try {
+            const client = new MongoClient(connectionString, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            await client.connect(err => {
+                const collection = client.db("button").collection("buttons");
+                collection.findOne({ urlId: req.params.urlId }, function (err, button) {
+                    if (err) throw err;
+                    if (!button) {
+                        res.status(404).json({ message: "Button not found" });
+                    } else {
+                        let userId = req.cookies.userId;
+                        if (!button.usersArray.includes(userId)) {
+                            console.log(userId)
+                            collection.updateOne({ urlId: req.params.urlId }, {
+                                $inc: { count: 1 },
+                                $push: { usersArray: req.cookies.userId }
+                            }, function(err, result) {
+                                if (err) throw err;
+                                res.status(200).json({ message: "Button count updated" });
+                                client.close();
+                            });
+                    } else {
+                        res.status(401).json({ message: "Already clicked!" });
+                    }
+                    }
+                });
+            });
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    });
+buttonRoutes.route('/api/button/reset/:urlId')
+    .patch(async (req, res) => {
+        try {
+
+        } catch (err) {
+            console.error('Error resetting click count:', err);
+        }
+
+        try {
+            const client = new MongoClient(connectionString, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            await client.connect(err => {
+                const collection = client.db("button").collection("buttons");
+                collection.findOne({ urlId: req.params.urlId }, function (err, button) {
+                    if (err) throw err;
+                    if (!button) {
+                        res.status(404).json({ message: "Button not found" });
+                    } else {
+                        let userId = req.cookies.userId;
+                        if (button.usersArray[0] === userId) {
+                            collection.updateOne({ urlId: req.params.urlId },
+                              { $set: { count: 0, usersArray: [userId, ]  } },
+                              function (err, result) {
+                                  if (err) throw err;
+                                  res.status(200).json({ message: "Button count updated" });
+                                  client.close();
+                              });
+                        } else {
+                            res.status(401).json({ message: "Unauthorized" });
+                        }
+                    }
+                });
+            });
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+
+    });
+module.exports = buttonRoutes;
---
+++ server.js
@@ -0,0 +1,77 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+const httpServer = http.listen(process.env.PORT || 5000, () => {
+  console.log('listening on *:5000');
+});
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('increment', (data) => {
+    io.emit('setLoading', true);
+    setTimeout(() => {
+      io.emit('setLoading', false);
+    }, 1500);
+    io.emit('snackbar', { message: 'Hello World!' });
+    io.emit('increment', data);
+  });
+  socket.on('reset', (data) => {
+    io.emit('setLoading', true);
+    setTimeout(() => {
+      io.emit('setLoading', false);
+    }, 1500);
+    io.emit('snackbar', { message: 'Hello World!' });
+    io.emit('reset', data);
+  });
+});
+
+// Loads the configuration from config.env to process.env
+require('dotenv').config({ path: './config.env' });
+const cookieParser = require('cookie-parser');
+const express = require('express');
+// const express = require('express');
+const cors = require('cors');
+// get MongoDB driver connection
+const dbo = require('./db/conn');
+const PORT = process.env.PORT || 5000;
+// const app = express();
+
+require('./models/ButtonSchema');
+require('./models/UserSchema');
+
+app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.header('origin') );
+  next();
+});
+app.use(cors({
+  origin: function(origin, callback){
+    return callback(null, true);
+  },
+  optionsSuccessStatus: 200,
+  credentials: true
+}));
+app.use(express.json());
+app.use(require('./routes/button'));
+// Global error handling
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+});
+
+// perform a database connection when the server starts
+dbo.connectToServer(function (err) {
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
+  // start the Express server
+  // app.listen(PORT, () => {
+  //   console.log(`Server is running on port: ${PORT}`);
+  // });
+});
