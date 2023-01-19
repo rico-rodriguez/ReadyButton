bc66b508c07c878065a5e4126378d7b7a3a47bdf
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-19 15:59:32 +0900

    realm added

--- schema/buttonSchema.js
+++ schema/buttonSchema.js
@@ -1,9 +1,9 @@
 const uuid = require('uuid');
 const {uuidV4} = require("mongodb/lib/core/utils");
 class Button extends Realm.Object {
-    @Realm.PrimaryKey()
     _id = new uuidV4();
     count = 0;
     urlId = '';
     usersArray = [];
 }
+Button.primaryKey = '_id';
