04f8891f7c14035cbe0502e9ecde7ffd9304141d
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-19 12:48:59 +0900

    realm added

--- server.js
+++ server.js
@@ -30,14 +30,14 @@ io.on('connection', (socket) => {
   });
 });
 const app = new Realm.App({ id: 'readybtn-fvinc' });
-const credentials = Realm.Credentials.anonymous();
-try {
-  const user = await app.logIn(credentials);
-  console.log("Successfully logged in!", user.id);
-  return user;
-} catch (err) {
-  console.error("Failed to log in", err.message);
-}
+// const credentials = Realm.Credentials.anonymous();
+// try {
+//   const user = await app.logIn(credentials);
+//   console.log("Successfully logged in!", user.id);
+//   return user;
+// } catch (err) {
+//   console.error("Failed to log in", err.message);
+// }
 
 
 // Loads the configuration from config.env to process.env
