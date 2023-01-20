6883dbd737684dbeb088467dfa8bb2299c1c113c
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-20 15:01:53 +0900

    Login button

--- src/UserControl.js
+++ src/UserControl.js
@@ -4,12 +4,14 @@ import React from "react";
 
 export default function UserControl() {
 const [name, setName] = React.useState('');
-
+const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+const [userName, setUserName] = React.useState('');
 const handleChange = (event) => {
     setName(event.target.value)
 
 }
 const handleSubmit = async (event) => {
+
     event.preventDefault()
     console.log(name)
 //     submit username to server /login
@@ -23,6 +25,13 @@ const handleSubmit = async (event) => {
             body: JSON.stringify({username: name}),
             withCredentials: true, // should be there
             credentials: 'include' // should be there
+        }).then(response => response.json()).then(data => {
+            if(data.username){
+                setIsLoggedIn(true)
+                setUserName(data.name)
+            } else {
+                setIsLoggedIn(false)
+            }
         })
         const data = await response.json()
         console.log(data)
@@ -34,12 +43,13 @@ const handleSubmit = async (event) => {
 
     return (
         <div style={{ position: 'fixed', top: '20px', right: '20px', backgroundColor:"white", borderRadius:"5px", padding:"10px" }}>
-            <FormControl>
+            {isLoggedIn ? <p>Welcome, {userName}</p> :
+                <FormControl>
                 <InputLabel style={{color:"black"}} htmlFor="my-input">User Name</InputLabel>
                 <Input id="my-input" aria-describedby="my-helper-text"  value={name} onChange={handleChange}/>
                 <FormHelperText id="my-helper-text">You need a username to participate.</FormHelperText>
                 <Button size="small" variant="outlined" onClick={handleSubmit} style={{color:"#010202", display:"block", width:"100%"}}>Submit</Button>
-            </FormControl>
+            </FormControl>}
         </div>
     );
 }
