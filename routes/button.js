3e0ce39e0129ba761c6ad10b5bdd2c72ca1484b6
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-20 15:14:59 +0900

    Login button

--- routes/button.js
+++ routes/button.js
@@ -44,7 +44,7 @@ buttonRoutes.route('/').get(async function (req, res) {
     if (req.cookies.user) {
         userId = req.cookies.user;
         console.log("previous user connected with id: " + userId);
-        res.json({isLoggedIn: true});
+        res.json({isLoggedIn: true, username: userId});
     }
     else {
         res.json({isLoggedIn: false});
@@ -59,7 +59,7 @@ buttonRoutes.route('/login').post(async function (req, res) {
     if (cookie === undefined) {
         res.cookie('user', credentials.username, { maxAge: 900000, httpOnly: true });
         console.log('cookie created successfully');
-        res.json({isLoggedIn: true});
+        res.json({isLoggedIn: true, username: userId});
     }
     else {
         console.log('cookie exists', cookie);
