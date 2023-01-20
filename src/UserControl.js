a869d68e592a8ddbb0d5deacaa555d5d0575f6fc
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-20 14:01:23 +0900

    Login button

--- src/UserControl.js
+++ src/UserControl.js
@@ -15,12 +15,12 @@ const handleSubmit = (event) => {
 }
 
     return (
-        <div style={{ position: 'fixed', top: '20px', right: '20px' }}>
-            <FormControl  style={{backgroundColor:"white", borderRadius:"5px"}}>
-                <InputLabel style={{color:"black" ,paddingTop:"10px"}} htmlFor="my-input">User Name</InputLabel>
+        <div style={{ position: 'fixed', top: '20px', right: '20px', backgroundColor:"white", borderRadius:"5px", padding:"10px" }}>
+            <FormControl>
+                <InputLabel style={{color:"black"}} htmlFor="my-input">User Name</InputLabel>
                 <Input id="my-input" aria-describedby="my-helper-text"  value={name} onChange={handleChange}/>
                 <FormHelperText id="my-helper-text">You need a username to participate.</FormHelperText>
-                <Button onClick={handleSubmit} style={{color:"#010202", display:"block", width:"100%"}}>Submit</Button>
+                <Button size="small" variant="outlined" onClick={handleSubmit} style={{color:"#010202", display:"block", width:"100%"}}>Submit</Button>
             </FormControl>
         </div>
     );
