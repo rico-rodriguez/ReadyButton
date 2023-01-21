2784e411ebc1f35d7073f557a87877e7f6604c0e
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-21 16:15:39 +0900

    Updating cookie to validate routes

--- src/App.js
+++ src/App.js
@@ -36,6 +36,28 @@ const theme = createTheme({
         },
     },
 });
+function clientCheckAuth(next) {
+    fetch('https://readybutton.herokuapp.com/api/check-session', {
+        method: 'GET',
+        credentials: 'include',
+
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*', // This is required for CORS support to work
+            'Access-Control-Allow-Credentials': 'true', // This is required for cookies, authorization headers with HTTPS
+        },
+    }).then(res => {
+        if(res.status === 401){
+            window.location.href = '/';
+            alert("Session expired, please login again.");
+        }
+        else{
+            next();
+        }
+    }).catch(err => {
+        console.error(err);
+    });
+}
 
 function App() {
     const [loggedIn, setIsLoggedIn] = useState(false);
@@ -49,7 +71,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>} />
                 {/*<Route path="/login" element={<LoginPage />} />*/}
-                {loggedIn && <Route path="/:urlId" element={<ButtonClicker />} /> }
+                {loggedIn && <Route path="/:urlId"  render={() => clientCheckAuth(<ButtonClicker />)} /> }
             </Routes>
         </BrowserRouter>
     </ThemeProvider>
