01eb5125e0ed9625a24d7025094da7804da33617
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-21 10:22:37 +0900

    Login button

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
-import React, { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
-import { useNavigate } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { Routes, Route, BrowserRouter, useNavigate  } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import ButtonClicker from './ButtonClicker';
 import Home from './Home';
+import {createTheme, ThemeProvider} from "@mui/material";
 
-const useStyles = makeStyles({
-    splash: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        zIndex: 9999,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+// Request cookies to see if user is logged in
+const theme = createTheme({
+    palette: {
+        primary: {
+            foreground: '#000000',
+            main: '#fff',
+        },
+        neutral: {
+            main: '#64748B',
+            contrastText: '#fff',
+        },
+        root: {
+            background: 'red'
+        }
+    },
+    components: {
+        // Name of the component
+        MuiCircularProgress: {
+            defaultProps: {
+                thickness: 1.6,
+            },
+            styleOverrides: {
+                circle: {
+                    color: '#58c549',
+                },
+            },
+        },
     },
 });
-
 function App() {
-    const [cookies] = useCookies(['user']);
     const navigate = useNavigate();
-    const classes = useStyles();
-    const [showSplash, setShowSplash] = React.useState(true);
-
     useEffect(() => {
-        if (cookies.user) {
-            setShowSplash(false);
-        } else {
+        // check for cookie
+        const cookie = document.cookie;
+        if (!cookie) {
             navigate('/');
         }
-    }, [cookies.user, navigate]);
-
+    }, [navigate]);
     return (
-        <>
-            {showSplash && (
-                <div className={classes.splash}>
-                    <Home />
-                </div>
-            )}
-            {!showSplash && <ButtonClicker />}
-        </>
+<ThemeProvider theme={theme}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/:urlId" element={<ButtonClicker />} />
+            </Routes>
+        </BrowserRouter>
+    </ThemeProvider>
     );
 }
 
