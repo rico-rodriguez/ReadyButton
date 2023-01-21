e146652b69bdbd0a9652c0cfaa35c3238dce9f53
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-21 10:45:00 +0900

    Login button

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     Button,
@@ -13,12 +14,10 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
         alignItems: 'center',
         margin: theme.spacing(2),
-
     },
     formControl: {
         width: '100%',
         margin: theme.spacing(2),
-        backgroundColor: 'rgba(255,255,255,0.65)',
     },
     submitButton: {
         margin: theme.spacing(2),
@@ -31,64 +30,69 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
     const classes = useStyles();
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const navigate = useNavigate();
+useEffect(() => {
+    // check for cookie
+    const cookie = document.cookie;
+    if (cookie) {
+        navigate('/');
+    }
+}, [navigate]);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setIsLoading(true);
-        try {
-            const response = await fetch('https://readybutton.herokuapp.com/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
-            const data = await response.json();
-            if (!response.ok) {
-                throw new Error(data.message);
-            }
-            // successfully logged in, do something with the returned data
-            // such as setting a cookie or redirecting to a different page
-        } catch (err) {
-            setError(err.message);
-        } finally {
-            setIsLoading(false);
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+        const response = await fetch('https://readybutton.herokuapp.com/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username }),
+        });
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(data.message);
         }
-    };
+        // set the cookie
+        document.cookie = `username=${data.username}`;
+        // redirect to the previous page
+        navigate(data.redirectUrl);
+    } catch (err) {
+        setError(err.message);
+    } finally {
+        setIsLoading(false);
+    }
+};
 
-    return (
-        <div className={classes.root}>
-            <Typography variant="h5">Log In</Typography>
-            {error && <Typography color="error">{error}</Typography>}
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    className={classes.formControl}
-                    label="Username"
-                    variant="outlined"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <TextField
-                    className={classes.formControl}
-                    label="Password"
-                    variant="outlined"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button
-                    className={classes.submitButton}
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    disabled={isLoading}
-                >
-                    {isLoading ? <CircularProgress className={classes.progress} /> : 'Log In'}
-                </Button>
-            </form>
-        </div>
-    );
+return (
+    <div className={classes.root}>
+        <Typography variant="h5">Log In</Typography>
+        {error && <Typography color="error">{error}</Typography>}
+        <form onSubmit={handleSubmit}>
+            <TextField
+                className={classes.formControl}
+                label="Username"
+                variant="outlined"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <Button
+                className={classes.submitButton}
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={isLoading}
+            >
+                {isLoading ? (
+                    <CircularProgress className={classes.progress} />
+                ) : (
+                    'Log In'
+                )}
+            </Button>
+        </form>
+    </div>
+);
 }
