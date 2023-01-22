79aa9d3f7effb520aff4e5be229bf550cc2d33fa
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 05:00:39 +0900

    Updating cookie to validate routes

--- src/PostMessage.js
+++ src/PostMessage.js
@@ -64,7 +64,7 @@ function PostMessage() {
                 </form>
 
         </div> }
-    <div style={{ position: 'relative', top:'40px', left: '50%', backgroundColor:"white", borderRadius:"5px", padding:"10px" }}>
+    <div style={{ position: 'absolute', top:'40px', left: '50%', backgroundColor:"white", borderRadius:"5px", padding:"10px" }}>
                 <div>Messages:
                 <div>{message}</div></div>
             </div>
