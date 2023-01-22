e8ef1338d9ef6979ce319494d64154b9af6dbfe2
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 03:47:30 +0900

    Updating cookie to validate routes

--- routes/button.js
+++ routes/button.js
@@ -68,7 +68,7 @@ buttonRoutes.route('/api/users').get(async (req, res) => {
         await client.connect();
       const collection = client.db("button").collection("buttons");
       collection.findOne({urlId: req.params.urlId},async (err, result) => {
-        const currentUser = await result.usersArray;
+        const currentUser = await result;
         console.log(currentUser)
         res.json(currentUser);
         await client.close();
