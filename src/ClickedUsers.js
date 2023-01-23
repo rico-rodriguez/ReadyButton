b72466bddc7d73e7a4e92ee24fb9c68227637b8e
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 10:29:52 +0900

    Updating cookie to validate routes

--- src/ClickedUsers.js
+++ src/ClickedUsers.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PerfectScrollbar from "perfect-scrollbar";
+import * as socket from "react-use/lib/misc/util";
 export default function ClickedUsers() {
   const [clickedUsers, setClickedUsers] = useState([]);
   const [usersList, setUsersList] = useState([]);
@@ -37,6 +38,11 @@ export default function ClickedUsers() {
       setUsersList(usersList);
     }
   }, [clickedUsers]);
+  useEffect(() => {
+    socket.on('users update', (data) => {
+      setClickedUsers(data.usersArray);
+    });
+  }, []);
 
   return (
     <>
