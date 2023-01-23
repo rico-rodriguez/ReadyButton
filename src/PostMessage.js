e8dad10c4f01c1b94fcef0368e475ba89aabd973
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 15:48:32 +0900

    Updating cookie to validate routes

--- src/PostMessage.js
+++ src/PostMessage.js
@@ -71,77 +71,61 @@ function PostMessage() {
 
   return (
     <>
-      {loading && <Skeleton />}
-      {currentButtonOwner === currentUser && (
-        <div
-          style={{
-            position: "fixed",
-            top: "12px",
-            left: "20px",
-            backgroundColor: "white",
-            borderRadius: "5px"
-          }}
-        >
-          <Button
-            variant="contained"
-            onClick={handleClick}
-          >
-            Post a message
-          </Button>
-          <Popover
-            sx={{ padding: "20px" }}
-            open={Boolean(anchorEl)}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "center"
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "center"
-            }}
-          >
-            <form onSubmit={handleSubmit}>
-          <textarea
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+      return (
+      <div style={{ display: loading ? "none" : "block" }}>
+        {currentButtonOwner === currentUser && (
+          <div
             style={{
-              width: "400px", height: "30px", padding: "10px", borderRadius: "5px", border: "1px solid black",
-              fontSize: "16px", fontFamily: "sans-serif", outline: "none", backgroundColor: "white",
-              color: "black", margin: "10px"
+              position: "fixed",
+              top: "12px",
+              left: "20px",
+              backgroundColor: "white",
+              borderRadius: "5px"
             }}
-          />
-              <button type="submit">Post</button>
-            </form>
-            {message.length > 0 && (
-              <div>
-                <div>Message: {message}</div>
-              </div>
-            )}
-          </Popover>
-          {/*<form onSubmit={handleSubmit}>*/}
-          {/*  <textarea*/}
-          {/*    value={message}*/}
-          {/*    onChange={(e) => setMessage(e.target.value)}*/}
-          {/*  />*/}
-          {/*  <button type="submit">Post</button>*/}
-          {/*</form>*/}
-        </div>
-      )}
-      {message.length > 0 && (
-        <div
-          style={{
-            display: "flex",
-            minWidth: "400px",
-            backgroundColor: "white",
-            borderRadius: "5px",
-            padding: "10px"
-          }}
-        >
-          <div>Message: {message}</div>
-        </div>
-      )}
+          >
+            <Button
+              variant="contained"
+              onClick={handleClick}
+            >
+              Post a message
+            </Button>
+            <Popover
+              sx={{ padding: "20px" }}
+              open={Boolean(anchorEl)}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center"
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center"
+              }}
+            >
+              <form onSubmit={handleSubmit}>
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              style={{
+                width: "400px", height: "30px", padding: "10px", borderRadius: "5px", border: "1px solid black",
+                fontSize: "16px", fontFamily: "sans-serif", outline: "none", backgroundColor: "white",
+                color: "black", margin: "10px"
+              }}
+            />
+                <button type="submit">Post</button>
+              </form>
+              {message.length > 0 && (
+                <div>
+                  <div>Message: {message}</div>
+                </div>
+              )}
+            </Popover>
+          </div>
+        )}
+        {loading && <Skeleton />}
+      </div>
+      )
     </>
   );
 }
