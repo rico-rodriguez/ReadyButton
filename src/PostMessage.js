a4e5903618ece25385304b37a2220685218507c0
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 11:36:46 +0900

    Updating cookie to validate routes

--- src/PostMessage.js
+++ src/PostMessage.js
@@ -1,12 +1,14 @@
-import { Dialog, DialogContent, DialogActions, Button } from "@mui/material";
-import { useEffect, useState } from "react";
-import * as socket from "react-use/lib/misc/util";
+import { useState, useEffect } from "react";
+import { Button } from "@mui/material";
+const io = require("socket.io-client");
+const socket = io("https://readybutton.herokuapp.com", {
+  withCredentials: true,
+});
 
 function PostMessage() {
   const [message, setMessage] = useState("");
   const [currentButtonOwner, setCurrentButtonOwner] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
-  const [open, setOpen] = useState(false);
 
   useEffect(async () => {
     // Listen for new messages from the server
@@ -41,21 +43,12 @@ function PostMessage() {
     }
   }, []);
 
-  function handleOpen() {
-    setOpen(true);
-  }
-
-  function handleClose() {
-    setOpen(false);
-  }
-
   function handleSubmit(e) {
     e.preventDefault();
     // Send the message to the server via Socket.io
     sendMessage(message);
     console.log(message, currentUser);
     setMessage("");
-    handleClose();
   }
 
   function sendMessage(messageData) {
@@ -66,23 +59,24 @@ function PostMessage() {
   return (
     <>
       {currentButtonOwner === currentUser && (
-        <>
-          <Button onClick={handleOpen}>Post message</Button>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogContent>
-              <form onSubmit={handleSubmit}>
-                <textarea
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-              </form>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={handleSubmit}>Post</Button>
-            </DialogActions>
-          </Dialog>
-        </>
+        <div
+          style={{
+            position: "fixed",
+            top: "20px",
+            left: "20px",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            padding: "10px",
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button type="submit">Post</button>
+          </form>
+        </div>
       )}
       {message.length > 0 && (
         <div
