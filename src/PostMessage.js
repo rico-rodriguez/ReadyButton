bf6adc11d57c9c357c5ac9b490f99c366fbbb64d
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-24 02:15:13 +0900

    Updating cookie to validate routes

--- src/PostMessage.js
+++ src/PostMessage.js
@@ -53,7 +53,7 @@ function PostMessage() {
       };
       setTimeout(() => {
         fetchUsers();
-      }, 1000);
+      }, 5000);
     }
   }, [currentUser]);
 
@@ -71,56 +71,74 @@ function PostMessage() {
 
   return (
     <>
-      {loading ? <Skeleton /> : <div> {currentButtonOwner === currentUser && (
+      <div style={{ display: loading ? "none" : "block" }}>
+        {currentButtonOwner === currentUser && (
+          <div
+            style={{
+              position: "fixed",
+              top: "12px",
+              left: "20px",
+              backgroundColor: "white",
+              borderRadius: "5px"
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={handleClick}
+            >
+              Post a message
+            </Button>
+            <Popover
+              sx={{ padding: "20px" }}
+              open={Boolean(anchorEl)}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center"
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center"
+              }}
+            >
+              <form onSubmit={handleSubmit}>
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              style={{
+                width: "400px", height: "30px", padding: "10px", borderRadius: "5px", border: "1px solid black",
+                fontSize: "16px", fontFamily: "sans-serif", outline: "none", backgroundColor: "white",
+                color: "black", margin: "10px"
+              }}
+            />
+                <button type="submit">Post</button>
+              </form>
+              {message.length > 0 && (
+                <div>
+                  <div>{message}</div>
+                </div>
+              )}
+            </Popover>
+          </div>
+        )}
+      </div>
+      {loading && <Skeleton />}
+      {message.length > 0 && (
         <div
           style={{
             position: "fixed",
-            top: "12px",
-            left: "20px",
-            backgroundColor: "white",
-            borderRadius: "5px"
+            top: "115px",
+            display: "flex",
+            minWidth: "400px",
+            backgroundColor: "#92979fc7",
+            borderRadius: "5px",
+            padding: "10px"
           }}
         >
-          <Button
-            variant="contained"
-            onClick={handleClick}
-          >
-            Post a message
-          </Button>
-          <Popover
-            sx={{ padding: "20px" }}
-            open={Boolean(anchorEl)}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "center"
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "center"
-            }}
-          >
-            <form onSubmit={handleSubmit}>
-                <textarea
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  style={{
-                    width: "400px", height: "30px", padding: "10px", borderRadius: "5px", border: "1px solid black",
-                    fontSize: "16px", fontFamily: "sans-serif", outline: "none", backgroundColor: "white",
-                    color: "black", margin: "10px"
-                  }}
-                />
-              <button type="submit">Post</button>
-            </form>
-            {message.length > 0 && (
-              <div>
-                <div>{message}</div>
-              </div>
-            )}
-          </Popover>
+          <div>{message}</div>
         </div>
-      )}</div>}
+      )}
     </>
   );
 }
