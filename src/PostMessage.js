96723f152b189e6a52f3012554cb0090b46fdcc5
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 12:27:09 +0900

    Updating cookie to validate routes

--- src/PostMessage.js
+++ src/PostMessage.js
@@ -99,9 +99,9 @@ function PostMessage() {
           <textarea
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            style={{width: "400px", height: "200px", padding: "10px", borderRadius: "5px", border: "1px solid black",
+            style={{width: "400px", height: "100px", padding: "10px", borderRadius: "5px", border: "1px solid black",
               fontSize: "16px", fontFamily: "sans-serif", outline: "none", backgroundColor: "white",
-              color: "black", marginLeft: "5px"}}
+              color: "black", margin: "10px"}}
           />
               <button type="submit">Post</button>
             </form>
