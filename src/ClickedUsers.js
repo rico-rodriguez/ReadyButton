458af1828dd60e590a005055a4daafa98ba97221
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-23 11:09:38 +0900

    Updating cookie to validate routes

--- src/ClickedUsers.js
+++ src/ClickedUsers.js
@@ -52,7 +52,7 @@ export default function ClickedUsers() {
             backgroundColor: "rgba(255,255,255,0.52)",
             borderRadius: "5px",
             padding: "20px",
-            overflow: "scroll",
+            overflow: "hidden",
             height: "500px",
             width: "fit-content",
             fontSize: "20px",
