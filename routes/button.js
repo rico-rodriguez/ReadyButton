11d0b4840f73c01d6341d0378109b6ff28de188d
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-27 06:44:05 +0900

    Add spinner.js & overlay to login

--- routes/button.js
+++ routes/button.js
@@ -12,23 +12,24 @@ const app = express();
 app.use('/', buttonRoutes);
 
 class ButtonSchema extends Realm.Object {
-  static schema = {
-    name: "Button",
-    properties: {
-      _id: { type: 'objectId', default: () => new Realm.BSON.ObjectId() },
-      count: "int",
-      urlId: "string",
-      usersArray: {type: 'list', objectType: 'string'}
-    },
-    primaryKey: '_id',
-  };
+    static schema = {
+        name: "Button",
+        properties: {
+            _id: {type: 'objectId', default: () => new Realm.BSON.ObjectId()},
+            count: "int",
+            urlId: "string",
+            usersArray: {type: 'list', objectType: 'string'}
+        },
+        primaryKey: '_id',
+    };
 }
+
 buttonRoutes.route('/login').post(async function (req, res) {
     // Extract the username from the request body
-    const { username } = req.body;
+    const {username} = req.body;
 
     // Log in the user and get the user object
-    const app = new Realm.App({ id: 'readybtn-fvinc' });
+    const app = new Realm.App({id: 'readybtn-fvinc'});
     const loginPayload = {username: req.body.username};
     const credentials = Realm.Credentials.function(loginPayload);
     const user = await app.logIn(credentials);
@@ -41,16 +42,16 @@ buttonRoutes.route('/logout').post(async function (req, res) {
 
 
     res.json({isLoggedIn: false});
-} );
+});
 
 
 buttonRoutes.route('/api/check-session').get(async (req, res) => {
     if (localStorage.getItem('username')) {
-        res.json({ loggedIn: true, username: localStorage.getItem('username') });
+        res.json({loggedIn: true, username: localStorage.getItem('username')});
     } else {
-        res.json({ loggedIn: false });
+        res.json({loggedIn: false});
     }
-} );
+});
 
 //initial page load
 buttonRoutes.route('/api/user/id').get(async (req, res) => {
@@ -66,13 +67,13 @@ buttonRoutes.route('/api/users').get(async (req, res) => {
             useUnifiedTopology: true,
         });
         await client.connect();
-      const collection = client.db("button").collection("buttons");
-      collection.findOne({urlId: req.query.urlId},async (err, result) => {
-        const currentUser = await result;
-        console.log(currentUser)
-        res.json(currentUser);
-        await client.close();
-      });
+        const collection = client.db("button").collection("buttons");
+        collection.findOne({urlId: req.query.urlId}, async (err, result) => {
+            const currentUser = await result;
+            console.log(currentUser)
+            res.json(currentUser);
+            await client.close();
+        });
     } catch (err) {
         console.error('Error getting current user for message posting:', err);
     }
@@ -91,7 +92,7 @@ buttonRoutes.route("/api/button/:urlId").get((req, res) => {
         return;
     } else {
         let auth = req.headers.authorization;
-        if( auth !== undefined && auth.startsWith("Bearer ")){
+        if (auth !== undefined && auth.startsWith("Bearer ")) {
             auth = auth.slice(7);
             username = auth;
         }
@@ -103,12 +104,12 @@ buttonRoutes.route("/api/button/:urlId").get((req, res) => {
             client.close();
         } else {
             const collection = client.db("button").collection("buttons");
-            collection.findOne({urlId: req.params.urlId},(err,result)=>{
+            collection.findOne({urlId: req.params.urlId}, (err, result) => {
                 if (err) {
                     console.log(err);
                     res.status(500).send(err);
                     client.close();
-                }else {
+                } else {
                     if (result) {
                         res.json(result);
                         client.close();
@@ -122,7 +123,7 @@ buttonRoutes.route("/api/button/:urlId").get((req, res) => {
                             if (err) {
                                 console.log(err);
                                 res.status(500).send(err);
-                            }else {
+                            } else {
                                 console.log("Button created successfully");
                                 res.json(newButton);
                             }
@@ -134,7 +135,6 @@ buttonRoutes.route("/api/button/:urlId").get((req, res) => {
         }
     });
 });
-
 
 
 buttonRoutes.route('/api/button/increment/:urlId')
@@ -150,65 +150,67 @@ buttonRoutes.route('/api/button/increment/:urlId')
                 useUnifiedTopology: true,
             });
             const username = req.headers.authorization.split(' ')[1];
+            const token = req.headers.authorization.split(' ')[2];
+            console.log(token);
             await client.connect(err => {
                 const collection = client.db("button").collection("buttons");
-                collection.findOne({ urlId: req.params.urlId }, (err, button) => {
+                collection.findOne({urlId: req.params.urlId}, (err, button) => {
                     if (err) throw err;
                     if (!button) {
-                        res.status(404).json({ message: "Button not found" });
+                        res.status(404).json({message: "Button not found"});
                     } else {
-                        if (!button.usersArray.includes(username)) {
+                        if (!button.usersArray.includes(token)) {
                             console.log(username + " is not in the array");
-                            collection.updateOne({ urlId: req.params.urlId }, {
-                                $inc: { count: 1 },
-                                $push: { usersArray: username }
-                            }, function(err, result) {
+                            collection.updateOne({urlId: req.params.urlId}, {
+                                $inc: {count: 1},
+                                $push: {usersArray: token + ' ' + username}
+                            }, function (err, result) {
                                 if (err) throw err;
-                                res.status(200).json({ message: "Button count updated" });
+                                res.status(200).json({message: "Button count updated"});
                                 client.close();
                             });
                         } else {
-                            res.status(401).json({ message: "Already clicked!" });
+                            res.status(401).json({message: "Already clicked!"});
                         }
                     }
                 });
             });
         } catch (err) {
-            res.status(400).json({ message: err.message });
+            res.status(400).json({message: err.message});
         }
     });
 
 buttonRoutes.route('/api/button/reset/:urlId')
-  .patch(async (req, res) => {
-    try {
-      const client = new MongoClient(connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      const username = req.headers.authorization.split(' ')[1];
-      await client.connect(err => {
-        const collection = client.db("button").collection("buttons");
-        collection.findOne({ urlId: req.params.urlId }, (err, button) => {
-          if (err) throw err;
-          if (!button) {
-            res.status(404).json({ message: "Button not found" });
-          } else {
-            if (button.usersArray[0] === username) {
-              collection.updateOne({ urlId: req.params.urlId }, {
-                $set: { count: 0, usersArray: [username] }
-              }, function(err, result) {
-                if (err) throw err;
-                res.status(200).json({ message: "Button reset" });
-                client.close();
-              });
-            } else {
-              res.status(401).json({ message: "Only the owner can reset the button!" });
-            }
-          }
-        });
-      });
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
+    .patch(async (req, res) => {
+        try {
+            const client = new MongoClient(connectionString, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+            const username = req.headers.authorization.split(' ')[1];
+            await client.connect(err => {
+                const collection = client.db("button").collection("buttons");
+                collection.findOne({urlId: req.params.urlId}, (err, button) => {
+                    if (err) throw err;
+                    if (!button) {
+                        res.status(404).json({message: "Button not found"});
+                    } else {
+                        if (button.usersArray[0] === username) {
+                            collection.updateOne({urlId: req.params.urlId}, {
+                                $set: {count: 0, usersArray: [username]}
+                            }, function (err, result) {
+                                if (err) throw err;
+                                res.status(200).json({message: "Button reset"});
+                                client.close();
+                            });
+                        } else {
+                            res.status(401).json({message: "Only the owner can reset the button!"});
+                        }
+                    }
+                });
+            });
+        } catch (err) {
+            res.status(400).json({message: err.message});
+        }
     });
 module.exports = buttonRoutes;
