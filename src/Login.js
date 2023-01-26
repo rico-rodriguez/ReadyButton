fe126afbefd5a11385d099615d762cf7963acb8e
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-01-27 04:31:34 +0900

    Updating cookie to validate routes

--- src/Login.js
+++ src/Login.js
@@ -102,7 +102,7 @@ export default function Login() {
       }}
     >
       <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
-        {isLoading ? <CircularProgress size={50} color="secondary" />
+        {isLoading ? <CircularProgress size={250} thickness={20} color="secondary" />
           : null}
       </div>
       {isLoggedIn ? (
