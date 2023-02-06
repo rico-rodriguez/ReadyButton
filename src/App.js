9b3ed9ff0af3324e2734b528a22a6268e5a9ffb8
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2023-02-07 00:46:38 +0900

    Add router animation

--- src/App.js
+++ src/App.js
@@ -1,60 +1,67 @@
-import {Routes, Route, BrowserRouter, useNavigate} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import ButtonClicker from './ButtonClicker';
-import Home from './Home';
-import LoginPage from './LoginPage';
-import {createTheme, ThemeProvider} from "@mui/material";
-import {useEffect, useState} from "react";
+import ButtonClicker from "./ButtonClicker";
+import Home from "./Home";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { useEffect, useState } from "react";
+import { AnimatedSwitch } from "react-router-transition";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            foreground: '#000000',
-            main: '#fff',
-        },
-        neutral: {
-            main: '#64748B',
-            contrastText: '#fff',
-        },
-        root: {
-            background: 'red'
-        }
+  palette: {
+    primary: {
+      foreground: "#000000",
+      main: "#fff"
     },
-    components: {
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff"
+    },
+    root: {
+      background: "red"
+    }
+  },
+  components: {
 // Name of the component
-        MuiCircularProgress: {
-            defaultProps: {
-                thickness: 1.6,
-            },
-            styleOverrides: {
-                circle: {
-                    color: '#58c549',
-                },
-            },
-        },
-    },
+    MuiCircularProgress: {
+      defaultProps: {
+        thickness: 1.6
+      },
+      styleOverrides: {
+        circle: {
+          color: "#58c549"
+        }
+      }
+    }
+  }
 });
 
 function App() {
-    const [loggedIn, setIsLoggedIn] = useState(false);
-    useEffect(() => {
-        if (localStorage.getItem('username')) {
-            setIsLoggedIn(true);
-        } else {
-            setIsLoggedIn(false);
-        }
-    }, []);
-    return (
-        <ThemeProvider theme={theme}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home/>} />
-                    {/*<Route path="/login" element={<LoginPage />} />*/}
-                    {loggedIn && <Route path="/:urlId" element={<ButtonClicker/>}/> }
-                        </Routes>
-                        </BrowserRouter>
-                        </ThemeProvider>
-                        );
-                    }
+  const [loggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("username")) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <AnimatedSwitch
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+            className="switch-wrapper"
+          >
+            <Route path="/" element={<Home />} />
+            {/*<Route path="/login" element={<LoginPage />} />*/}
+            {loggedIn && <Route path="/:urlId" element={<ButtonClicker />} />}
+          </AnimatedSwitch>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
 
-                    export default App;
+export default App;
